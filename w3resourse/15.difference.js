faa6486d8453e2e6b213a454d4786a7bba08c85d
bhagyashree-ghude <bhagyashreeghude@gmail.com>
2024-03-16 15:48:20 +0530

    finding difference

---
+++ w3resourse/15.difference.js
@@ -0,0 +1,10 @@
+const number = prompt("enter number")
+const difference = number-13
+
+if(number >13){
+    const result= 2*difference
+    console.log("result ", result)
+}
+else if(number <13){
+    console.log("number is less than 13")
+}
